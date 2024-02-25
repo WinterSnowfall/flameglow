4d1413081a39c9973dca5fafbca3df55e24fbd0e
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2024-02-25 18:08:18 +0200

    Add temp detection for NVMe SSDs + more lenient sensor detection logic

--- scripts/flameglow.py
+++ scripts/flameglow.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''
 @author: Winter Snowfall
-@version: 2.10
-@date: 12/02/2024
+@version: 2.20
+@date: 24/02/2024
 
 Warning: Built for use with python 3.6+
 '''
@@ -17,16 +17,17 @@ from os_stats import os_stats
 # conf file block
 CONF_FILE_PATH = os.path.join('..', 'conf', 'flameglow.conf')
 
+NVME_DEVICE_NAME = 'nvme'
 SUPPORTED_GPU_TYPES = ('nvidia', 'amd', 'raspberrypi')
 
 def sigterm_handler(signum, frame):
     print('Stopping stats collection due to SIGTERM...')
-    
+
     raise SystemExit(0)
 
 def sigint_handler(signum, frame):
     print('Stopping stats collection due to SIGINT...')
-    
+
     raise SystemExit(0)
 
 if __name__ == '__main__':
@@ -34,15 +35,15 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, sigterm_handler)
     # catch SIGINT and exit gracefully
     signal.signal(signal.SIGINT, sigint_handler)
-    
+
     print(f'Starting flameglow - a simple POSIX proc system stat collection agent...\n\n')
-    
+
     configParser = ConfigParser()
-    
+
     try:
         configParser.read(CONF_FILE_PATH)
         general_section = configParser['GENERAL']
-        
+
         PROMETHEUS_CLIENT_PORT = general_section.getint('prometheus_client_port')
         STATS_COLLECTION_INTERVAL = general_section.getint('collection_interval')
         NET_INTF_NAME = general_section.get('network_interface_name')
@@ -54,9 +55,9 @@ if __name__ == '__main__':
     except:
         print('Could not parse configuration file. Please make sure the appropriate structure is in place!')
         raise SystemExit(1)
-    
+
     ### Prometheus client metrics ###############################################################################
-    
+
     #----------------------------------------------- os_stats ---------------------------------------------------
     proc_stats_avg_cpu_usage = Gauge('proc_stats_avg_cpu_usage', 'Average CPU usage over the last minute')
     proc_stats_memory_load = Gauge('proc_stats_memory_load', 'Current RAM memory usage')
@@ -66,28 +67,30 @@ if __name__ == '__main__':
     proc_stats_io_read_rate = Gauge('proc_stats_io_read_rate', 'Bytes read on the specified io device')
     proc_stats_io_write_rate = Gauge('proc_stats_io_write_rate', 'Bytes written on the specified io device')
     #------------------------------------------------------------------------------------------------------------
-    
+
     #------------------------------------------------ sys_stats -------------------------------------------------
     sys_stats_cpu_package_temp = Gauge('sys_stats_cpu_package_temp', 'Current CPU package temperature')
     if GPU_TYPE in SUPPORTED_GPU_TYPES:
         sys_stats_gpu_temp = Gauge('sys_stats_gpu_temp', 'Current GPU temperature')
+    if NVME_DEVICE_NAME in IO_DEV_NAME:
+        sys_stats_nvme_composite_temp = Gauge('sys_stats_nvme_composite_temp', 'Current NVME composite temperature')
     #------------------------------------------------------------------------------------------------------------
-    
+
     #############################################################################################################
-    
+
     # start a Prometheus http server thread to expose the metrics
     start_http_server(PROMETHEUS_CLIENT_PORT)
-    
+
     os_stats_inst = os_stats(HOST_TYPE, GPU_TYPE, LOGGING_LEVEL)
     os_stats_inst.set_net_intf_name(NET_INTF_NAME)
     os_stats_inst.set_io_device_name(IO_DEV_NAME)
-    
+
     terminate_signal = False
-    
+
     while not terminate_signal:
         try:
             os_stats_inst.collect_stats()
-            
+
             proc_stats_avg_cpu_usage.set(os_stats_inst.avg_cpu_usage)
             proc_stats_memory_load.set(os_stats_inst.memory_load)
             proc_stats_uptime.set(os_stats_inst.uptime)
@@ -96,18 +99,20 @@ if __name__ == '__main__':
             proc_stats_trans_rate.set(os_stats_inst.net_trans_rate / STATS_COLLECTION_INTERVAL)
             proc_stats_io_read_rate.set(os_stats_inst.io_bytes_read / STATS_COLLECTION_INTERVAL)
             proc_stats_io_write_rate.set(os_stats_inst.io_bytes_written / STATS_COLLECTION_INTERVAL)
-            
+
             sys_stats_cpu_package_temp.set(os_stats_inst.cpu_package_temp)
             if GPU_TYPE in SUPPORTED_GPU_TYPES:
                 sys_stats_gpu_temp.set(os_stats_inst.gpu_temp)
-            
+            if NVME_DEVICE_NAME in IO_DEV_NAME:
+                sys_stats_nvme_composite_temp.set(os_stats_inst.nvme_composite_temp)
+
             sleep(STATS_COLLECTION_INTERVAL)
-            
+
         except SystemExit:
             print('Stopping flameglow...')
             terminate_signal = True
 
         except:
-            os_stats_inst.clear_stats() 
+            os_stats_inst.clear_stats()
 
     print(f'\n\nThank you for using flameglow. Bye!')
--- scripts/os_stats.py
+++ scripts/os_stats.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''
 @author: Winter Snowfall
-@version: 2.10
-@date: 12/02/2024
+@version: 2.20
+@date: 24/02/2024
 
 Warning: Built for use with python 3.6+
 '''
@@ -45,26 +45,28 @@ RPI_GPU_TEMP_COMMAND = ['vcgencmd', 'measure_temp']
 
 class os_stats:
     '''gather host stats using the /proc and /sys OS stat modules'''
-    
+
     _logging_level = logging.WARNING
-    
+
     def __init__(self, host_type, gpu_type, logging_level):
         self._net_intf_name = None
         self._net_intf_bytes_rec_prev = None
         self._net_intf_bytes_trans_prev = None
         self._net_intf_bytes_rec = 0
         self._net_intf_bytes_trans = 0
-        
+
         self._io_device_name = None
         self._io_device_sectors_read_prev = None
         self._io_device_sectors_written_prev = None
         self._io_device_sectors_read = 0
         self._io_device_sectors_written = 0
-        
-        self._thermal_zone_identifier = 0
-        self._gpu_card_identifier = 0
-        self._hwmon_folder_identifier = 0
-        
+
+        self._cpu_thermal_zone_id = None
+        self._nvme_drive_id = None
+        self._nvme_hwmon_id = None
+        self._gpu_card_id = None
+        self._gpu_hwmon_id = None
+
         self.avg_cpu_usage = 0
         self.memory_load = 0
         self.uptime = 0
@@ -72,95 +74,125 @@ class os_stats:
         self.net_trans_rate = 0
         self.io_bytes_read = 0
         self.io_bytes_written = 0
-        
+
         self.cpu_package_temp = 0
+        self.nvme_composite_temp = 0
         self.gpu_temp = 0
-        
+
         # defaults to 'WARNING' otherwise
         if logging_level == 'DEBUG':
             self._logging_level = logging.DEBUG
         elif logging_level == 'INFO':
             self._logging_level = logging.INFO
-        
+
         self._host_type = host_type
         self._gpu_type = gpu_type
-        
+
         # logging level for current logger
         logger.setLevel(self._logging_level)
-        
-        self.detect_thermal_zone_path()
-        
+
+        self.detect_cpu_thermal_zone_path()
+        self.detect_nvme_path()
         if self._gpu_type == GPU_TYPES[1]:
             self.detect_gpu_path()
-    
+
     def set_net_intf_name(self, net_intf_name):
         self._net_intf_name = net_intf_name
-    
+
     def set_io_device_name(self, io_device_name):
         self._io_device_name = io_device_name
-    
-    def detect_thermal_zone_path(self):
+
+    def detect_cpu_thermal_zone_path(self):
         logger.info(f'Detecting CPU package thermal zone for {self._host_type} host type...')
-        
+
         thermal_zone_no = 0
-        
+
         while os.path.exists(f'/sys/class/thermal/thermal_zone{thermal_zone_no}'):
             logger.debug(f'Atempting CPU package thermal zone detection for: {thermal_zone_no}...')
-            
+
             with open(f'/sys/class/thermal/thermal_zone{thermal_zone_no}/type', 'r') as zone_type:
                 detected_zone_type = zone_type.read().strip()
                 logger.debug(f'detected_zone_type: {detected_zone_type}')
-                
+
                 if self._host_type != SYS_RASPBERRY_PI_HOST_TYPE:
                     if detected_zone_type == SYS_CPU_THERMAL_ZONE_TYPE_GENERIC:
-                        self._thermal_zone_identifier = thermal_zone_no
+                        self._cpu_thermal_zone_id = thermal_zone_no
                         logger.info('Succesfully detected CPU package thermal zone.')
                         return
                 else:
                     if detected_zone_type == SYS_CPU_THERMAL_ZONE_TYPE_PI:
-                        self._thermal_zone_identifier = thermal_zone_no
+                        self._cpu_thermal_zone_id = thermal_zone_no
                         logger.info('Succesfully detected CPU package thermal zone.')
                         return
-                
+
                 thermal_zone_no += 1
-        
-        # early returns in case of detection will guarantee this is only hit when something goes wrong
-        logger.critical('Thermal zones have been exhausted without detection.')
-        raise SystemExit(2)
-    
+
+        logger.warning('CPU thermal zones have been exhausted without detection.')
+
+    def detect_nvme_path(self):
+        logger.info(f'Detecting NVMe composite thermal readings...')
+
+        nvme_no = 0
+
+        while os.path.exists(f'/sys/class/nvme/nvme{nvme_no}'):
+            logger.debug(f'Atempting NVMe hwmon detection for nvme: {nvme_no}...')
+
+            with os.scandir(f'/sys/class/nvme/nvme{nvme_no}') as hwmon_path:
+                for hwmon_path_entry in hwmon_path:
+                    if hwmon_path_entry.name.startswith('hwmon') and hwmon_path_entry.is_dir():
+                        logger.debug(f'Atempting NVMe temp input detection for: {hwmon_path_entry.name}...')
+
+                        if os.path.exists(f'/sys/class/nvme/nvme{nvme_no}/{hwmon_path_entry.name}/temp1_input'):
+                            self._nvme_drive_id = nvme_no
+                            logger.debug(f'nvme_no: {nvme_no}')
+
+                            detected_hwmon_no = hwmon_path_entry.name[5:]
+                            self._nvme_hwmon_id = detected_hwmon_no
+                            logger.debug(f'detected_hwmon_no: {detected_hwmon_no}')
+
+                            logger.info('Succesfully detected NVMe hwmon path.')
+                            return
+
+            nvme_no += 1
+
+        logger.info('No NVMe devices with thermal readings have been detected.')
+
     def detect_gpu_path(self):
-        logger.info(f'Detecting GPU package thermal zone for {self._gpu_type} GPU type...')
-        
+        logger.info(f'Detecting GPU thermal readings for {self._gpu_type} GPU type...')
+
         card_no = 0
-        hwmon_no = 0
-        
+
         while os.path.exists(f'/sys/class/drm/card{card_no}'):
             logger.debug(f'Atempting GPU card detection for card: {card_no}...')
-            
-            while os.path.exists(f'sys/class/drm/card{card_no}/device/hwmon/hwmon{hwmon_no}'):
-                logger.debug(f'Atempting GPU card detection for hwmon: {hwmon_no}...')
-                
-                with open(f'/sys/class/drm/card{card_no}/device/hwmon/hwmon{hwmon_no}/name', 'r') as card_name:
-                    detected_card_name = card_name.read().strip()
-                    logger.debug(f'detected_card_name: {detected_card_name}')
-                    
-                    if detected_card_name in SYS_GPU_CARD_TYPES:
-                        self._gpu_card_identifier = card_no
-                        self._hwmon_folder_identifier = hwmon_no
-                        logger.info('Succesfully detected GPU card.')
-                        return
-        
-        # early returns in case of detection will guarantee this is only hit when something goes wrong
-        logger.critical('DRM cards have been exhausted without detection.')
-        raise SystemExit(3)
-    
+
+            with os.scandir(f'sys/class/drm/card{card_no}/device/hwmon') as hwmon_path:
+                for hwmon_path_entry in hwmon_path:
+                    if hwmon_path_entry.name.startswith('hwmon') and hwmon_path_entry.is_dir():
+                        logger.debug(f'Atempting GPU card detection for: {hwmon_path_entry.name}...')
+
+                        detected_hwmon_no = hwmon_path_entry.name[5:]
+                        logger.debug(f'detected_hwmon_no: {detected_hwmon_no}')
+
+                        with open(f'/sys/class/drm/card{card_no}'
+                                  f'/device/hwmon/hwmon{detected_hwmon_no}/name', 'r') as card_name:
+                            detected_card_name = card_name.read().strip()
+                            logger.debug(f'detected_card_name: {detected_card_name}')
+
+                            if detected_card_name in SYS_GPU_CARD_TYPES:
+                                self._gpu_card_id = card_no
+                                self._gpu_hwmon_id = detected_hwmon_no
+                                logger.info('Succesfully detected GPU card.')
+                                return
+
+        logger.warning('No DRM cards with thermal readings have been detected.')
+
     def clear_stats(self):
         self._net_intf_bytes_rec = 0
         self._net_intf_bytes_trans = 0
-        
+
         self._io_device_sectors_read = 0
         self._io_device_sectors_written = 0
-        
+
         self.avg_cpu_usage = 0
         self.memory_load = 0
         self.uptime = 0
@@ -168,24 +200,26 @@ class os_stats:
         self.net_trans_rate = 0
         self.io_bytes_read = 0
         self.io_bytes_written = 0
-        
+
         self.cpu_package_temp = 0
-    
+        self.nvme_composite_temp = 0
+        self.gpu_temp = 0
+
     def collect_stats(self):
         logger.info('***** Starting data collection run *****')
-        
+
         try:
             # /proc/loadavg file parsing
             with open(PROC_LOADAVG_PATH, 'r') as loadavg:
                 self.avg_cpu_usage = loadavg.read().split()[0]
-                
+
                 logger.debug(f'avg_cpu_usage: {self.avg_cpu_usage}')
-            
+
             # /proc/meminfo file parsing
             with open(PROC_MEMINFO_PATH, 'r') as meminfo:
                 memory_total = 0
                 memory_available = 0
-                
+
                 for line in meminfo.read().splitlines():
                     if line.startswith('MemTotal'):
                         memory_total = line.split(':')[1].strip().split()[0]
@@ -193,17 +227,17 @@ class os_stats:
                         memory_available = line.split(':')[1].strip().split()[0]
                     if memory_total != 0 and memory_available != 0:
                         break
-                
+
                 self.memory_load = int(memory_total) - int(memory_available)
-                
+
                 logger.debug(f'memory_load: {self.memory_load}')
-            
+
             # /proc/uptime file parsing
             with open(PROC_UPTIME_PATH, 'r') as uptime:
                 self.uptime = int(float(uptime.read().split()[0]))
-                
+
                 logger.debug(f'uptime: {self.uptime}')
-            
+
             # /proc/net/dev file parsing
             with open(PROC_NET_DEV_PATH, 'r') as net_dev:
                 for line in net_dev.read().splitlines():
@@ -212,13 +246,13 @@ class os_stats:
                         self._net_intf_bytes_rec = int(intf_line[0])
                         self._net_intf_bytes_trans = int(intf_line[8])
                         break
-                
+
                 logger.debug(f'_net_intf_bytes_rec: {self._net_intf_bytes_rec}')
                 logger.debug(f'_net_intf_bytes_trans: {self._net_intf_bytes_trans}')
-                
+
                 logger.debug(f'_net_intf_bytes_rec_prev: {self._net_intf_bytes_rec_prev}')
                 logger.debug(f'_net_intf_bytes_trans_prev: {self._net_intf_bytes_trans_prev}')
-                
+
                 # won't do a delta on the first pass, so return 0
                 if self._net_intf_bytes_rec_prev is None and self._net_intf_bytes_trans_prev is None:
                     self.net_rec_rate = 0
@@ -230,10 +264,10 @@ class os_stats:
                 # setup delta for next iteration
                 self._net_intf_bytes_rec_prev = self._net_intf_bytes_rec
                 self._net_intf_bytes_trans_prev = self._net_intf_bytes_trans
-                
+
                 logger.debug(f'net_rec_rate: {self.net_rec_rate}')
                 logger.debug(f'net_trans_rate: {self.net_trans_rate}')
-            
+
             # /proc/diskstats file parsing
             with open(PROC_IO_DEV_PATH, 'r') as io_dev:
                 for line in io_dev.read().splitlines():
@@ -243,81 +277,99 @@ class os_stats:
                         self._io_device_sectors_read = int(intf_line[5])
                         self._io_device_sectors_written = int(intf_line[9])
                         break
-                
+
                 logger.debug(f'_io_device_sectors_read: {self._io_device_sectors_read}')
                 logger.debug(f'_io_device_sectors_written: {self._io_device_sectors_written}')
-                
+
                 logger.debug(f'_io_device_sectors_read_prev: {self._io_device_sectors_read_prev}')
                 logger.debug(f'_io_device_sectors_written_prev: {self._io_device_sectors_written_prev}')
-                
+
                 # won't do a delta on the first pass, so return 0
                 if self._io_device_sectors_read_prev is None and self._io_device_sectors_written_prev is None:
                     self.io_bytes_read = 0
                     self.io_bytes_written = 0
                 else:
-                    self.io_bytes_read = (self._io_device_sectors_read - 
+                    self.io_bytes_read = (self._io_device_sectors_read -
                                           self._io_device_sectors_read_prev) * IO_SECTOR_SIZE
-                    self.io_bytes_written = (self._io_device_sectors_written - 
+                    self.io_bytes_written = (self._io_device_sectors_written -
                                              self._io_device_sectors_written_prev) * IO_SECTOR_SIZE
-                
+
                 # setup delta for next iteration
                 self._io_device_sectors_read_prev = self._io_device_sectors_read
                 self._io_device_sectors_written_prev = self._io_device_sectors_written
-                
+
                 logger.debug(f'io_bytes_read: {self.io_bytes_read}')
                 logger.debug(f'io_bytes_written: {self.io_bytes_written}')
-            
+
             # /sys/class/thermal/thermal_zone*/temp parsing
-            with open(f'/sys/class/thermal/thermal_zone{self._thermal_zone_identifier}/temp', 'r') as temp:
-                self.cpu_package_temp = int(temp.read())
-                
-                logger.debug(f'cpu_package_temp: {self.cpu_package_temp}')
-            
+            if self._cpu_thermal_zone_id is not None:
+                with open(f'/sys/class/thermal/thermal_zone{self._cpu_thermal_zone_id}/temp', 'r') as temp:
+                    self.cpu_package_temp = int(temp.read())
+
+                    logger.debug(f'cpu_package_temp: {self.cpu_package_temp}')
+            else:
+                logger.debug('Skipping CPU package temperature collection.')
+
+            # /sys/class/nvme/nvme*/hwmon0/temp1_input parsing
+            if self._nvme_drive_id is not None and self._nvme_hwmon_id is not None:
+                # temp1_input is traditionally the "composite" temperature
+                # of the NVMe drive, which is used for throttling
+                with open(f'/sys/class/nvme/nvme{self._nvme_drive_id}'
+                          f'/hwmon{self._nvme_hwmon_id}/temp1_input', 'r') as nvme_temp:
+                    self.nvme_composite_temp = int(nvme_temp.read())
+
+                logger.debug(f'nvme_composite_temp: {self.nvme_composite_temp}')
+            else:
+                logger.debug('Skipping NVMe composite temperature collection.')
+
             # nvidia-smi command output parsing
             if self._gpu_type == GPU_TYPES[0]:
                 try:
                     # use the nvidia-smi utility to parse temperature for nvidia
-                    nvidia_smi_output = subprocess.run(NVIDIA_GPU_TEMP_COMMAND, capture_output=True, 
+                    nvidia_smi_output = subprocess.run(NVIDIA_GPU_TEMP_COMMAND, capture_output=True,
                                                        text=True, check=True)
-                    
+
                     # multiply by 1000 to align with sys sensor readings default format
                     self.gpu_temp = int(nvidia_smi_output.stdout.strip()) * 1000
                 except:
                     self.gpu_temp = 0
                     logger.warning('Nvidia SMI could not communicate with the Nvidia driver.')
-                
+
                 logger.debug(f'gpu_temp: {self.gpu_temp}')
-            
+
             # /sys/class/drm/card*/device/hwmon/hwmon*/temp1_input file parsing
             elif self._gpu_type == GPU_TYPES[1]:
-                with open(f'/sys/class/drm/card{self._gpu_card_identifier}/device'
-                          f'/hwmon/hwmon{self._hwmon_folder_identifier}/temp1_input', 
-                          'r') as temp:
-                    self.gpu_temp = int(temp.read())
-                    
-                    logger.debug(f'gpu_temp: {self.gpu_temp}')
-                    
+                if self._gpu_card_id is not None and self._gpu_hwmon_id is not None:
+                    with open(f'/sys/class/drm/card{self._gpu_card_id}/device'
+                              f'/hwmon/hwmon{self._gpu_hwmon_id}/temp1_input',
+                              'r') as temp:
+                        self.gpu_temp = int(temp.read())
+
+                        logger.debug(f'gpu_temp: {self.gpu_temp}')
+                else:
+                    logger.debug('Skipping GPU temperature collection.')
+
             # vcgencmd measure_temp command output parsing
             elif self._gpu_type == GPU_TYPES[2]:
                 try:
                     # use the vcgencmd utility to parse temperature for Raspberry Pis
-                    vcgencmd_output = subprocess.run(RPI_GPU_TEMP_COMMAND, capture_output=True, 
+                    vcgencmd_output = subprocess.run(RPI_GPU_TEMP_COMMAND, capture_output=True,
                                                      text=True, check=True)
-                    
+
                     # multiply by 1000 to align with sys sensor readings default format
                     self.gpu_temp = int(float(vcgencmd_output.stdout.strip().split('=')[1][:-2]) * 1000)
-                    
+
                 except:
                     self.gpu_temp = 0
                     logger.warning('Unable to retrieve Raspberry Pi GPU temperature from vcgencmd.')
-                
+
                 logger.debug(f'gpu_temp: {self.gpu_temp}')
-                
+
             else:
                 logger.debug('No supported GPU type detected. Skipping GPU temp collection.')
-            
+
             logger.info('***** Data collection complete *****')
-        
+
         except Exception as exception:
             logger.error(f'Encountered following exception: {type(exception)} {exception}')
             # uncomment for debugging purposes only
